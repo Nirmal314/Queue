ce5bd2de12fa17bcf2dad56c80b7e1ff564aa56b
Nirmal <85055463+Nirmal314@users.noreply.github.com>
2021-10-26 11:17:31 +0530

    Update Queue.c

--- Queue.c
+++ Queue.c
@@ -8,6 +8,11 @@ typedef struct
     int front, rear;
 } queue;
 
+void init(queue *queue)
+{
+    queue->front = queue->rear = -1;
+    printf("Queue initiallized.\n\n");
+}
 queue *createQueue(int size)
 {
     if (size <= 0)
@@ -18,8 +23,8 @@ queue *createQueue(int size)
     {
         queue *queue = malloc(sizeof(size * sizeof(queue)));
         queue->front = queue->rear = -1;
-        printf("Queue created of size %d.\n", size);
-        printf("Queue initiallized.\n");
+        printf("\nQueue created of size %d.\n", size);
+        printf("Queue initiallized.\n\n");
         return queue;
     }
 }
@@ -28,17 +33,19 @@ void enqueue(queue *queue, int val)
 {
     if (queue->rear == MAX - 1)
     {
-        printf("Queue overflow.\n");
+        printf("\nQueue overflow.\n\n");
     }
     else if (queue->front && queue->rear == -1)
     {
         queue->front = queue->rear = 0;
         queue->data[queue->rear] = val;
+        printf("\n%d enqueued !\n\n", val);
     }
     else
     {
         queue->rear++;
         queue->data[queue->rear] = val;
+        printf("\n%d enqueued !\n\n", val);
     }
 }
 int dequeue(queue *queue)
@@ -46,12 +53,14 @@ int dequeue(queue *queue)
     int val;
     if (queue->front && queue->rear == -1 || queue->front > queue->rear)
     {
-        printf("Queue underflow.\n");
+        printf("\nQueue underflow.\n\n");
+        return 0;
     }
     else
     {
         val = queue->data[queue->front];
         queue->front++;
+        printf("\n%d dequeued !\n\n", val);
         return val;
     }
 }
@@ -60,38 +69,82 @@ int peek(queue *queue)
     int val;
     if (queue->front && queue->rear == -1 || queue->front > queue->rear)
     {
-        printf("Queue underflow.\n");
+        printf("\nQueue underflow.\n\n");
     }
     else
     {
         val = queue->data[queue->front];
+        printf("\nFront value is %d.\n\n", val);
         return val;
     }
 }
 void display(queue *queue)
 {
+    printf("\n");
     for (int i = queue->front; i <= queue->rear; i++)
     {
         printf("%d <- ", queue->data[i]);
     }
-    printf("\n");
+    printf("\n\n");
 }
 void change(queue *queue, int pos, int val)
 {
     int index = queue->front + pos - 1;
+    int bef;
     if (index >= queue->front && index <= queue->rear)
     {
+        bef = queue->data[index];
         queue->data[index] = val;
+        printf("\nValue changed to %d from %d !\n\n", val,bef);
     }
     else
     {
-        printf("Invalid position.\n");
+        printf("\nInvalid position.\n\n");
     }
 }
+
 void main()
 {
-    queue *queue = createQueue(2);
-    enqueue(queue, 10);
-    enqueue(queue, 20);
-    enqueue(queue, 20);
+    queue *queue = malloc(sizeof(queue) * MAX);
+    init(queue);
+    // queue *queue = createQueue(3);
+    int en, deq, c, p, pos, val;
+    while (1)
+    {
+        printf("1.Enqueue\n2.Dequeue\n3.Peek\n4.Change\n5.Display\n6.Exit\n");
+        printf("\nWhich operation you want to perform ? : ");
+        scanf("%d", &c);
+        switch (c)
+        {
+        case 1:
+            printf("Enter value : ");
+            scanf("%d", &en);
+            enqueue(queue, en);
+            break;
+        case 2:
+            deq = dequeue(queue);
+            break;
+        case 3:
+            p = peek(queue);
+            break;
+        case 4:
+            printf("Enter position : ");
+            scanf("%d", &pos);
+            printf("Enter new value : ");
+            scanf("%d", &val);
+            change(queue, pos, val);
+            break;
+        case 5:
+            display(queue);
+            break;
+        case 6:
+            printf("\nYour final queue is : \n");
+            display(queue);
+            exit(0);
+            break;
+        default:
+            printf("\nInvalid entry !\n\n");
+            break;
+        }
+    }
 }
